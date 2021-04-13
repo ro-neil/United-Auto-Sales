f3c60e9a35a162f80f7ad6a44667fa9169365ce1
Roneil Barrett <roneilbarrett@gmail.com>
2021-04-13 13:20:34 -0500

    Added Register Form template

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, TextAreaField, SelectField
+from wtforms.validators import InputRequired, DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class RegisterForm(FlaskForm):
+    title = StringField('Property Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    totalRooms = StringField('No. of Rooms', validators=[DataRequired()])
+    totalBathrooms = StringField('No. of Bathrooms', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
+    propertyType = SelectField('Property Type', choices=['House', 'Apartment'], validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+    photo = FileField('Photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')
+    ])
+    
